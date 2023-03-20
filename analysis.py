9306055026b1bbdff3904929e72b648c0c7b5c1a
Shane Keenan <10012926@atu.ie>
2023-03-20 22:53:51 +0000

    initial start

---
+++ analysis.py
@@ -0,0 +1,44 @@
+# analysis.py  
+# pands project   
+# Author: Shane Keenan
+# Status: ongoing 
+
+
+'''  Problem statement
+This project concerns the well-known Fisher’s Iris data set [3]. 
+You must research the data set and write documentation and code (in Python [1]) to investigate it.
+An online search for information on the data set will convince you that many people have investigated it previously. 
+
+You are expected to be able to break this project into several smaller tasks that are easier to solve,
+and to plug these together after they have been completed. 
+
+You might do that for this project as follows:
+
+1. Research the data set online and write a summary about it in your README. 
+2. Download the data set and add it to your repository. 
+3. Write a program called analysis.py that: 
+1. Outputs a summary of each variable to a single text file, 
+2. Saves a histogram of each variable to png files, and 
+3. Outputs a scatter plot of each pair of variables. 
+4. Performs any other analysis you think is appropriate
+
+
+You may produce a Jupyter notebook as well containing all your comment.
+This notebook should only contain text that you have written yourself, (it may contain referenced code from other sources). 
+I will harshly mark any text (not code) that I feel is not written directly by you. 
+I want to know what YOU think, not some third party. 
+
+It might help to suppose that your manager has asked you to investigate the data set, with a 
+view to explaining it to your colleagues. Imagine that you are to give a presentation on the 
+data set in a few weeks’ time, where you explain what investigating a data set entails and how 
+Python can be used to do it. 
+You have not been asked to create a deck of presentation slides, 
+but rather to present your code and its output to them. 
+
+'''
+
+
+
+
+
+
